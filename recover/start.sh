e517a54109963b131d1b2215014216c277dfeb46
pengan1990 <pengaan@163.com>
2019-08-05 12:01:22 +0800

    using  as para

--- recover/start.sh
+++ recover/start.sh
@@ -7,4 +7,6 @@ kill -9 $pid
 echo "结束{$pName}进程.."
 sleep 1
 echo "开始recover 进程"
-nohup ./recover -path=/mysql_backup -cluster_id=1000 -user=root -password=secret -dbreg="*" -tbreg="*" -time="2099-12-31 23:59:59" -level=debug > recover.log 2>&1 &
+# nohup ./recover -path=/mysql_backup -cluster_id=1000 -user=root -password=secret -dbreg="*" -tbreg="*" -time="2099-12-31 23:59:59" -level=debug > recover.log 2>&1 &
+
+nohup ./recover $@ &
